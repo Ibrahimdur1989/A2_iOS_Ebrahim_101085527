bac90c5be7411cb07f28fa74980766e150af3dc8
Ebrahim Al-Serri <ebrahim.al-serri@georgebrown.ca>
2026-04-09 22:49:17 -0400

    Improve navigation title font size

--- A2_iOS_Ebrahim_101085527/ContentView.swift
+++ A2_iOS_Ebrahim_101085527/ContentView.swift
@@ -77,6 +77,7 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Products")
+            .font(.title)
             .onAppear {
                 insertSampleProducts()
             }
